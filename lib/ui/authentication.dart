d27105f07ea5ea86ecd0a55892aee635aed99e0e
mafazasp <mafazasp@RETROBEAST>
2021-03-17 07:49:33 +0700

    auth page mockup, no method yet

--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -8,16 +8,74 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
+    return Scaffold(
+      body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           color: Colors.blueAccent,
         ),
-        child: Text("Hello world"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextFormField(
+              controller: _emailField,
+              decoration: InputDecoration(
+                hintText: "",
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextFormField(
+              controller: _passwordField,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "",
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                labelText: "Password",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
