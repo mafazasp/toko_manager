9a37f9f4383a6b1acfc6d137d76e2e043554ba07
mafazasp <mafazasp@gmal.com>
2022-02-15 16:16:31 +0700

    list test

--- lib/ui/inventory_add_view.dart
+++ lib/ui/inventory_add_view.dart
@@ -9,6 +9,27 @@ class InventoryAddView extends StatefulWidget {
 }
 
 class _InventoryAddViewState extends State<InventoryAddView> {
+  //inventory_add_form{
+  // product cart;
+  // int quantity;
+  // int total_price;
+  // int extra_costs;
+  // string payment_method;
+  // string added_by;
+  // string supplier_name;
+  // bool paid;
+  //
+  // if product_name does not exist in products collection:
+  //  showProductAddPopupForm() {
+  //    #similar to product_add_view
+  //    add data to products
+  //    redirect back to the inventory_add_view
+  // if product_name exist:
+  // }
+
+  final _formKey = GlobalKey<FormState>();
+  List<String> cart = ["product 1", "product 2"];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +37,13 @@ class _InventoryAddViewState extends State<InventoryAddView> {
         appBar: AppBar(),
         drawer: HomeDrawer(),
         body: Container(
-          child: Text('This is Inventory Add View!'),
+          child: ListView.builder(
+              itemCount: cart.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(cart[index]),
+                );
+              }),
         ),
       ),
     );
